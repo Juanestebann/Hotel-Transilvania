01ee9f4d5586ed5fbca925ea903cb44de83b5f35
jaelreyes <jae.reyesm@duocuc.cl>
2026-05-14 12:22:42 -0400

    actualizar logs

--- ms-reserva-service/src/main/java/com/example/ms_reserva_service/MsReservaServiceApplication.java
+++ ms-reserva-service/src/main/java/com/example/ms_reserva_service/MsReservaServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsReservaServiceApplication {
+public class   MsReservaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsReservaServiceApplication.class, args);
