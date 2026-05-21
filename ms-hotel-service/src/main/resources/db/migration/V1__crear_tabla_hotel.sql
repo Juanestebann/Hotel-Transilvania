167885d3e181767afa4d7fbd400756deea2b4730
Juanestebann <juanestebann14@gmail.com>
2026-05-21 18:20:20 -0400

    Solucion Flyway-Starter

--- ms-hotel-service/src/main/resources/db/migration/V1__crear_tabla_hotel.sql
+++ ms-hotel-service/src/main/resources/db/migration/V1__crear_tabla_hotel.sql
@@ -1,9 +1,9 @@
-CREATE TABLE HOTEL (
-    idHotel BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR (20) NOT NULL,
-    direccion VARCHAR (30) NOT NULL,
-    ciudad VARCHAR (20),
-    pais VARCHAR (15),
-    categoria VARCHAR (20) NOT NULL,
-    descripcion VARCHAR (50)
-)
+CREATE TABLE hotel (
+    idHotel BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    ciudad VARCHAR(50) NOT NULL,
+    pais VARCHAR(50) NOT NULL,
+    categoria VARCHAR(30) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL
+);
